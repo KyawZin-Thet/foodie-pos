21765444468cfd1dbec67e7fe458a44eeaaa86d3
KyawZin-Thet <128166677+KyawZin-Thet@users.noreply.github.com>
2024-01-05 08:33:02 +0630

    typeError/featurespath

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
 import { Box } from "@mui/material";
+import Features from "../components/Features";
 
 const FoodiePOS = () => {
   return (
